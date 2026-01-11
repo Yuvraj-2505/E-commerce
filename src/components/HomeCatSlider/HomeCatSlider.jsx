bf71f1db80d93ac1653a4bff913e441ca6f27aa2
Yuvraj Prasad <yuvrajprasad1590@gmail.com>
2026-01-11 20:47:09 +0530

    update in banner

--- src/components/HomeCatSlider/HomeCatSlider.jsx
+++ src/components/HomeCatSlider/HomeCatSlider.jsx
@@ -14,11 +14,11 @@ const HomeCatSlider = () => {
 
 
   return (
-    <div className="homeCatSlider py-3">
+    <div className="homeCatSlider pt-4 py-8">
       <div className="container relative !overflow-visible">
         <Swiper
           spaceBetween={10}
-          slidesPerView={7}
+          slidesPerView={8}
           modules={[Navigation]}
           onBeforeInit={(swiper) => {
             swiper.params.navigation.prevEl = prevRef.current;
@@ -41,10 +41,10 @@ const HomeCatSlider = () => {
             <Link to="/">
               <div className="item py-7 px-3 bg-white rounded-sm flex text-center flex justify-center flex-col items-center">
                 <img
-                  src="/images/Cat-Banner/1767337493085_20457.png"
+                  src="/images/Cat-Banner/1761905929738_file_1734525218436_ele.png"
                   className="w-[60px] transition-all"
                 />
-                <h3 className="text-[15px] font-[500] mt-3">Faishon</h3>
+                <h3 className="text-[15px] font-[500] mt-3">Electronics</h3>
               </div>
             </Link>
           </SwiperSlide>
@@ -52,10 +52,10 @@ const HomeCatSlider = () => {
             <Link to="/">
               <div className="item py-7 px-3 bg-white rounded-sm flex text-center flex justify-center flex-col items-center">
                 <img
-                  src="/images/Cat-Banner/1767337493085_20457.png"
+                  src="/images/Cat-Banner/1761905971086_file_1734525231018_bag.png"
                   className="w-[60px] transition-all"
                 />
-                <h3 className="text-[15px] font-[500] mt-3">Faishon</h3>
+                <h3 className="text-[15px] font-[500] mt-3">Bags</h3>
               </div>
             </Link>
           </SwiperSlide>
@@ -63,10 +63,10 @@ const HomeCatSlider = () => {
             <Link to="/">
               <div className="item py-7 px-3 bg-white rounded-sm flex text-center flex justify-center flex-col items-center">
                 <img
-                  src="/images/Cat-Banner/1767337493085_20457.png"
+                  src="/images/Cat-Banner/1761905982766_file_1734525239704_foot.png"
                   className="w-[60px] transition-all"
                 />
-                <h3 className="text-[15px] font-[500] mt-3">Faishon</h3>
+                <h3 className="text-[15px] font-[500] mt-3">Footwear</h3>
               </div>
             </Link>
           </SwiperSlide>
@@ -74,10 +74,10 @@ const HomeCatSlider = () => {
             <Link to="/">
               <div className="item py-7 px-3 bg-white rounded-sm flex text-center flex justify-center flex-col items-center">
                 <img
-                  src="/images/Cat-Banner/1767337493085_20457.png"
+                  src="/images/Cat-Banner/1761905996339_file_1734525248057_gro.png"
                   className="w-[60px] transition-all"
                 />
-                <h3 className="text-[15px] font-[500] mt-3">Faishon</h3>
+                <h3 className="text-[15px] font-[500] mt-3">Groceries</h3>
               </div>
             </Link>
           </SwiperSlide>
@@ -85,10 +85,10 @@ const HomeCatSlider = () => {
             <Link to="/">
               <div className="item py-7 px-3 bg-white rounded-sm flex text-center flex justify-center flex-col items-center">
                 <img
-                  src="/images/Cat-Banner/1767337493085_20457.png"
+                  src="/images/Cat-Banner/1761906005923_file_1734525255799_beauty(1).png"
                   className="w-[60px] transition-all"
                 />
-                <h3 className="text-[15px] font-[500] mt-3">Faishon</h3>
+                <h3 className="text-[15px] font-[500] mt-3">Beauty</h3>
               </div>
             </Link>
           </SwiperSlide>
@@ -96,10 +96,21 @@ const HomeCatSlider = () => {
             <Link to="/">
               <div className="item py-7 px-3 bg-white rounded-sm flex text-center flex justify-center flex-col items-center">
                 <img
-                  src="/images/Cat-Banner/1767337493085_20457.png"
+                  src="/images/Cat-Banner/1761906015678_file_1734525275367_well.png"
                   className="w-[60px] transition-all"
                 />
-                <h3 className="text-[15px] font-[500] mt-3">Faishon</h3>
+                <h3 className="text-[15px] font-[500] mt-3">Wellness</h3>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/">
+              <div className="item py-7 px-3 bg-white rounded-sm flex text-center flex justify-center flex-col items-center">
+                <img
+                  src="/images/Cat-Banner/1761906025549_file_1734525286186_jw.png"
+                  className="w-[60px] transition-all"
+                />
+                <h3 className="text-[15px] font-[500] mt-3">Jewellery</h3>
               </div>
             </Link>
           </SwiperSlide>
